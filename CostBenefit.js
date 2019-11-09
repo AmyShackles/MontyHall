fa439fb45a54e394ad657ed83aed4d33af59fa43
Amy Shackles <amyshackles@gmail.com>
2019-11-08 20:08:23 -0800

    Program to determine how many doors it takes before switching hurts you more often than helps you in batches of 1,000,000 and output of same

--- CostBenefit.js
+++ CostBenefit.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 function getDoorArray(numDoors) {
   let doors = [];
   for (let i = 0; i < numDoors; ) {
@@ -33,29 +35,42 @@ function montyHall(numDoors, guess) {
 }
 
 function main() {
-  let numDoors = 3,
-    costBenefit = true;
-  while (costBenefit) {
-    let losses = 0,
-      wins = 0,
-      hurtToSwitch = 0;
-    for (let i = 0; i < 10000000; i++) {
-      let guess = Math.floor(Math.random() * numDoors) + 1;
-      let result = montyHall(numDoors, guess);
-      if (result === "Switching helped") {
-        wins++;
-      } else if (result === "So close") {
-        hurtToSwitch++;
-      } else if (result === "Switching had no effect") {
-        losses++;
+  fs.open("output.md", "a", (err, fd) => {
+    if (err) throw err;
+    let numDoors = 3,
+      costBenefit = true;
+    while (costBenefit) {
+      let losses = 0,
+        wins = 0,
+        hurtToSwitch = 0;
+      for (let i = 0; i < 1000000; i++) {
+        let guess = Math.floor(Math.random() * numDoors) + 1;
+        let result = montyHall(numDoors, guess);
+        if (result === "Switching helped") {
+          wins++;
+        } else if (result === "So close") {
+          hurtToSwitch++;
+        } else if (result === "Switching had no effect") {
+          losses++;
+        }
       }
+      costBenefit = wins >= hurtToSwitch ? true : false;
+      const message = `With ${numDoors} doors, it helped to switch ${wins} times, hurt to switch ${hurtToSwitch} times, and had no effect ${losses} times\n`;
+      const file = fs.createWriteStream("output.md", { fd, autoClose: false });
+      file.on("error", function(err) {
+        console.error(err);
+      });
+      if (numDoors === 3) {
+        file.write("\n" + message);
+      } else {
+        file.write(message);
+      }
+      numDoors++;
     }
-    costBenefit = wins >= hurtToSwitch ? true : false;
-    console.log(
-      `With ${numDoors} doors, it helped to switch ${wins} times, hurt to switch ${hurtToSwitch} times, and had no effect ${losses} times`
-    );
-    numDoors++;
-  }
+    fs.close(fd, err => {
+      if (err) throw err;
+    });
+  });
   return;
 }
 main();
